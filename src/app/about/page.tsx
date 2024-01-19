61e0f6e3ba2daad03c4fd1c03a428d2c49be2c36
Biruk Kebede <biruk.key@gmail.com>
2024-01-19 21:50:50 +0000

    Directory based routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/about.module.css'
+
+export default function About() {
+	return (
+		<main className={styles['about-main']}>
+			<h1>About Page</h1>
+		</main>
+	)
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/projects.module.css'
+
+export default function Projects() {
+	return (
+		<main className={styles['projects-main']}>
+			<h1>Projects Page</h1>
+		</main>
+	)
+}
