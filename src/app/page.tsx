f820c2caa7a6fbaccb72b15c9797be7a81ad4a49
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2024-01-24 22:36:47 +0000

    Home page responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,34 +11,34 @@ export default async function Home() {
 	const content = await getProfile()
 
 	return (
-		<main className={styles.main}>
-			<div className={styles.hero}>
-				<div className={styles.container}>
-					<h1 className={styles.title}>{content.title}</h1>
-					<p>{content.intro}</p>
-				</div>
-			</div>
-			<section>
-				{content.sub_titles.map((title, index) => (
-					<div key={index}>
-						<h4>{title}</h4>
-						<div>
-							<Image
-								src={`/images/${content.images[index]}`}
-								alt={title}
-								loading='eager'
-								className={styles['tech-img']}
-								width={340}
-								height={240}
-								sizes='(min-width: 300px) 100vw'
-								placeholder='blur'
-								blurDataURL={`/images/${content.images[index]}`}
-							/>
-							<p>{content.description[index]}</p>
-						</div>
-					</div>
-				))}
-			</section>
-		</main>
-	)
+    <main className={styles.main}>
+      <section className={styles.hero}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>{content.title}</h1>
+          <p>{content.intro}</p>
+        </div>
+      </section>
+      <section>
+        {content.sub_titles.map((title, index) => (
+          <div key={index}>
+            <h4 className={styles["sub-title"]}>{title}</h4>
+            <div className={styles.development}>
+              <Image
+                src={`/images/${content.images[index]}`}
+                alt={title}
+                loading="eager"
+                className={styles["tech-img"]}
+                width={340}
+                height={240}
+                sizes="(min-width: 300px) 100vw"
+                placeholder="blur"
+                blurDataURL={`/images/${content.images[index]}`}
+              />
+              <p className={styles.description}>{content.description[index]}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 }
