e80638078458a384078d66f378ca4702783274a4
Biruk Kebede <biruk.key@gmail.com>
2024-01-24 20:58:30 +0000

    Fetching profile data and populating home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/globals.css'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootswatch/dist/sandstone/bootstrap.min.css'
+import '../styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,18 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<link
+					rel='stylesheet'
+					href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'
+					integrity='sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65'
+					crossOrigin='anonymous'
+				/>
+				<link
+					href='https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/sandstone/bootstrap.min.css'
+					rel='stylesheet'
+				/>
+			</head>
 			<body className={inter.className}>
 				<Navbar />
 				{children}
--- src/app/projects/[details]/[id]/page.tsx
+++ src/app/projects/[details]/[id]/page.tsx
@@ -1,53 +1,53 @@
-import prisma from "../../../../lib/db";
-import Image from "next/image";
-import styles from "../../../../styles/details.module.css";
+import prisma from '../../../../lib/db'
+import Image from 'next/image'
+import styles from '../../../../styles/details.module.css'
 
 export async function generateStaticParams() {
-  const projects = await prisma.project.findMany();
+	const projects = await prisma.project.findMany()
 
-  return projects.map((project) => [
-    {
-      details: project.title,
-      id: project.id,
-    },
-  ]);
+	return projects.map(project => [
+		{
+			details: project.title,
+			id: project.id,
+		},
+	])
 }
 
 async function getCurrentProject(params: number) {
-  const project = await prisma.project.findUnique({
-    where: {
-      id: params,
-    },
-  });
+	const project = await prisma.project.findUnique({
+		where: {
+			id: params,
+		},
+	})
 
-  return project;
+	return project
 }
 
 export default async function ProjectDetails({
-  params,
+	params,
 }: {
-  params: { details: string; id: string };
+	params: { details: string; id: string }
 }) {
-  const project = await getCurrentProject(Number(params.id));
-  return (
-    <div className={styles["details-main"]}>
-      <h1>Project Details</h1>
-      {project && (
-        <div className={styles["details-container"]}>
-          <h4>{project.title}</h4>
-          <Image
-            src={`/images/${project.image}`}
-            alt={project.title}
-            loading="eager"
-            className={styles["details-img"]}
-            width={340}
-            height={240}
-            sizes="(min-width: 300px) 100vw"
-            placeholder="blur"
-            blurDataURL={`/images/${project.image}`}
-          />
-        </div>
-      )}
-    </div>
-  );
+	const project = await getCurrentProject(Number(params.id))
+	return (
+		<div className={styles['details-main']}>
+			<h1>Project Details</h1>
+			{project && (
+				<div className={styles['details-container']}>
+					<h4>{project.title}</h4>
+					<Image
+						src={`/images/${project.image}`}
+						alt={project.title}
+						loading='eager'
+						className={styles['details-img']}
+						width={340}
+						height={240}
+						sizes='(min-width: 300px) 100vw'
+						placeholder='blur'
+						blurDataURL={`/images/${project.image}`}
+					/>
+				</div>
+			)}
+		</div>
+	)
 }
