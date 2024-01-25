054392063a6fb6b5bc44eadb6049c983132c525a
Biruk Kebede <biruk.key@gmail.com>
2024-01-25 21:15:02 +0000

    Hero section layout and Framer motion animation

---
+++ src/components/home.tsx
@@ -0,0 +1,120 @@
+'use client'
+import { useRef } from 'react'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import AnimatedText from './text-animation'
+import styles from '../styles/home.module.css'
+
+type HomePageProps = {
+	title: string
+	intro: string | null
+	sub_titles: string[]
+	images: string[]
+	description: string[]
+}
+
+export default function HomePage({
+	title,
+	intro,
+	sub_titles,
+	images,
+	description,
+}: HomePageProps) {
+	const ref = useRef(null)
+	const pageVariant = {
+		visible: {
+			opacity: 1,
+			y: 0,
+			transition: { type: 'spring', stiffness: 30, delay: 0.5 },
+		},
+		hidden: { opacity: 0, y: -300 },
+	}
+
+	const leftVariant = {
+		visible: {
+			opacity: 1,
+			x: 0,
+			y: 0,
+			transition: { type: 'spring', stiffness: 30 },
+		},
+		hidden: { opacity: 0, x: -300, y: 40 },
+	}
+
+	const rightVariant = {
+		visible: {
+			opacity: 1,
+			x: 0,
+			y: 0,
+			transition: { type: 'spring', stiffness: 30 },
+		},
+		hidden: { opacity: 0, x: 300, y: 40 },
+	}
+
+	return (
+		<motion.main
+			className={styles.main}
+			ref={ref}
+			variants={pageVariant}
+			initial='hidden'
+			whileInView='visible'
+		>
+			<section className={styles.hero}>
+				<Image
+					src={'/images/hero-bg.jpg'}
+					alt={title}
+					loading='eager'
+					className={styles['bg-img']}
+					width={340}
+					height={240}
+					sizes='(min-width: 300px) 100vw'
+					placeholder='blur'
+					blurDataURL={'hero-bg.jpg'}
+				/>
+				<div className={styles['overlay-text']}>
+					<AnimatedText text={title} className={styles.title} el={'h1'} once />
+				</div>
+			</section>
+			<div className={styles.wrapper}>
+				<p className={styles.intro}>{intro}</p>
+				<section>
+					{sub_titles.map((title: string, index: number) => (
+						<div key={index}>
+							<AnimatedText
+								text={title}
+								el={'h3'}
+								className={styles['sub-title']}
+							/>
+							<div className={styles.development}>
+								<motion.div
+									variants={leftVariant}
+									initial='hidden'
+									whileInView='visible'
+								>
+									<Image
+										src={`/images/${images[index]}`}
+										alt={title}
+										loading='eager'
+										className={styles['tech-img']}
+										width={340}
+										height={240}
+										sizes='(min-width: 300px) 100vw'
+										placeholder='blur'
+										blurDataURL={`/images/${images[index]}`}
+									/>
+								</motion.div>
+								<motion.p
+									variants={rightVariant}
+									initial='hidden'
+									whileInView='visible'
+									className={styles.description}
+								>
+									{description[index]}
+								</motion.p>
+							</div>
+						</div>
+					))}
+				</section>
+			</div>
+		</motion.main>
+	)
+}
