97f960e44d981c204098056827a5fd01142fc23e
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2024-01-28 19:58:49 +0000

    Refactoring react useState type and project details page layout

--- src/app/projects/[project-details]/page.tsx
+++ src/app/projects/[project-details]/page.tsx
@@ -1,42 +1,64 @@
-'use client'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import styles from '../../../styles/details.module.css'
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../../../styles/details.module.css";
+import AnimatedText from "@/components/text-animation";
 
-const projectState = history.state
+const projectState = history.state;
+
+type ProjectProps = {
+	id: number;
+	title: string;
+	image: string;
+	summary: string | null;
+	demo: string;
+};
 
 export default function ProjectDetails() {
-	const [project, setProject] = useState<{
-		id: number
-		title: string
-		image: string
-		summary: string | null
-		demo: string
-	}>({ id: 0, title: '', image: '', summary: '', demo: '' })
+	const initialState = { id: 0, title: "", image: "", summary: "", demo: "" };
+
+	const [project, setProject] = useState<ProjectProps>(initialState);
 
 	useEffect(() => {
-		setProject(projectState.state)
-	}, [])
+		setProject(projectState.state);
+	}, []);
 
 	return (
-		<div className={styles['details-main']}>
-			<h1 className={styles['project-name']}>{project.title}</h1>
+		<div className={styles["details-main"]}>
+			<AnimatedText
+				text={project.title}
+				el="h1"
+				delay={0.5}
+				className={styles["project-name"]}
+			/>
 			{project && (
-				<div className={styles['details-container']}>
+				<div className={styles["details-container"]}>
 					<Image
 						src={`/images/${project.image}`}
 						alt={project.title}
-						loading='eager'
-						className={styles['details-img']}
+						loading="eager"
+						className={styles["details-img"]}
 						width={340}
 						height={240}
-						sizes='(min-width: 300px) 100vw'
-						placeholder='blur'
+						sizes="(min-width: 300px) 100vw"
+						placeholder="blur"
 						blurDataURL={`/images/${project.image}`}
 					/>
-					<p>{project.summary ? project.summary : ''}</p>
+					<p className={styles["details-summary"]}>
+						{project.summary ? project.summary : ""}
+					</p>
 				</div>
 			)}
+			<div className={styles["link-container"]}>
+				<a
+					href={project.demo}
+					target="_blank"
+					rel="noreferrer"
+					className={styles["live-demo"]}
+				>
+					Visit {project.title}
+				</a>
+			</div>
 		</div>
-	)
+	);
 }
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,21 +1,21 @@
-'use client'
-import styles from '../styles/projects.module.css'
-import Image from 'next/image'
-import { motion } from 'framer-motion'
-import AnimatedText from './text-animation'
-import { useRouter } from 'next/navigation'
+"use client";
+import styles from "../styles/projects.module.css";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import AnimatedText from "./text-animation";
+import { useRouter } from "next/navigation";
 
 type ProjectProps = {
-	id: number
-	title: string
-	image: string
-	summary: string | null
-	demo: string
-	code: string
-}
+	id: number;
+	title: string;
+	image: string;
+	summary: string | null;
+	demo: string;
+	code: string;
+};
 
 export default function Projects({ projects }: { projects: ProjectProps[] }) {
-	const router = useRouter()
+	const router = useRouter();
 	const scrollVariant = {
 		visible: (index: number) => ({
 			opacity: 1,
@@ -26,67 +26,66 @@ export default function Projects({ projects }: { projects: ProjectProps[] }) {
 			},
 		}),
 		hidden: { opacity: 0, scale: 0 },
-	}
+	};
 
 	return (
-		<main className={styles['projects-main']}>
+		<main className={styles["projects-main"]}>
 			<AnimatedText
-				text='Check out our Projects'
-				el='h1'
+				text="Check out our Projects"
+				el="h1"
 				delay={3}
 				once
-				className={styles['page-title']}
+				className={styles["page-title"]}
 			/>
-			<div className={styles['projects-wrapper']}>
+			<div className={styles["projects-wrapper"]}>
 				{projects &&
 					projects.map((project: object | any, index: number) => (
 						<motion.div
 							variants={scrollVariant}
-							initial='hidden'
-							whileInView='visible'
+							initial="hidden"
+							whileInView="visible"
 							whileHover={{ scale: 1.1 }}
 							whileTap={{ scale: 0.9 }}
 							viewport={{ amount: 0.1 }}
 							custom={index}
 							key={index}
-							className={styles['project-wrapper']}
+							className={styles["project-wrapper"]}
 							onClick={() => {
-								history.pushState({ state: project }, '')
+								history.pushState({ state: project }, "");
 								router.push(
-									`projects/${project.title.toLowerCase().split(' ').join('-')}`
-								)
+									`projects/${project.title.toLowerCase().split(" ").join("-")}`
+								);
 							}}
 						>
-							<h4 className={styles['project-title']}>{project.title}</h4>
-							<div className={styles['image-tooltip-container']}>
+							<h4 className={styles["project-title"]}>{project.title}</h4>
+							<div className={styles["image-tooltip-container"]}>
 								<Image
 									src={`/images/${project.image}`}
 									alt={project.title}
-									loading='eager'
-									className={styles['project-img']}
+									loading="eager"
+									className={styles["project-img"]}
 									width={340}
 									height={240}
-									sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-									placeholder='blur'
+									sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+									placeholder="blur"
 									blurDataURL={`/images/${project.image}`}
 								/>
 								<span className={styles.tooltip}>
 									Click to view project details
 								</span>
 							</div>
-							<div className={styles['project-links-wrapper']}>
-								<a
-									href={project.demo}
-									target='_blank'
-									rel='noreferrer'
-									className={styles['live-demo-link']}
-								>
-									Live Demo
-								</a>
-							</div>
+
+							<a
+								href={project.demo}
+								target="_blank"
+								rel="noreferrer"
+								className={styles["live-demo-link"]}
+							>
+								Live Demo
+							</a>
 						</motion.div>
 					))}
 			</div>
 		</main>
-	)
+	);
 }
