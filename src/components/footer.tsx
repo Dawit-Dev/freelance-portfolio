28dcbeba2163c7f2d5fc19beb61bb8df95a0f7d1
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2024-01-29 17:37:16 +0000

    Adding footer site navigation and project images layout fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,22 @@
+"use client"
+
+import Link from 'next/link';
 import styles from '../styles/footer.module.css'
 
 const Footer = () => {
 	return (
-		<main className={styles['footer-main']}>
-			<h1>This is footer</h1>
+		<main className={styles["footer-main"]}>
+			<div className={styles["navigation-links"]}>
+				<h4>Site Navigation:</h4>
+				<Link href={"/"} className={styles["nav-link"]}>Home</Link>
+				<Link href={"/services"} className={styles["nav-link"]}>Services</Link>
+				<Link href={"/projects"} className={styles["nav-link"]}>Projects</Link>
+				<Link href={"/about"} className={styles["nav-link"]}>About</Link>
+				<Link href={"/contact"} className={styles["nav-link"]}>Contact</Link>
+			</div>
+			<div className={styles["social-media-links"]}></div>
 		</main>
-	)
+	);
 }
 
 export default Footer
