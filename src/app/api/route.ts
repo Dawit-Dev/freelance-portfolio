3980626ccec6a569d82bcdfcc649d7495f651012
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2024-02-09 22:48:05 +0000

    Bug fix: subject optional field

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,27 +1,30 @@
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function POST(req: Request) {
 	try {
-		const body = await req.json()
+		const body = await req.json();
 		const message = `
 		  Name: ${body.name}\r\n
 		  Email: ${body.email}\r\n
 		  Message: ${body.message}
-		`
+		`;
 		await sgMail.send({
 			to: process.env.RECEIVER_EMAIL,
 			from: process.env.SENDER_EMAIL,
 			subject: body.subject ? body.subject : `New message from ${body.name}`,
 			text: message,
-			html: `<strong>${message.replace(/\r\n/g, '<br />')}</strong>`,
-		})
+			html: `<strong>${message.replace(/\r\n/g, "<br />")}</strong>`,
+		});
 	} catch (err: any) {
-		let errorMessage: string = 'Something went wrong'
+		let errorMessage: string = "Something went wrong";
 		if (err instanceof Error) {
-			errorMessage = err.message
+			errorMessage = err.message;
 		}
-		return Response.json({ message: errorMessage })
+		return Response.json({ message: errorMessage });
 	}
-	return Response.json({ message: 'Your email is successfully sent' })
+	const response = Response.json({
+		message: "Your email is successfully sent",
+	});
+	return response;
 }
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,280 +1,282 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
-import { motion } from 'framer-motion'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faPaperPlane,
 	faPhoneVolume,
 	faEnvelope,
 	faMapLocationDot,
-} from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-import { ThemeProvider, createTheme } from '@mui/material/styles'
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 export const theme = createTheme({
 	components: {
 		MuiFormLabel: {
 			styleOverrides: {
 				asterisk: {
-					color: '#db3131',
-					'&$error': {
-						color: '#db3131',
+					color: "#db3131",
+					"&$error": {
+						color: "#db3131",
 					},
 				},
 			},
 		},
 	},
-})
+});
 
-import styles from '../styles/contact.module.css'
+import styles from "../styles/contact.module.css";
+
+type InputProps = {
+	name: string;
+	email: string;
+	subject: string;
+	message: string;
+};
 
 type ErrorProps = {
-	name: boolean
-	email: boolean | object | any
-	subject: boolean
-	message: boolean
-}
+	name: boolean;
+	email: boolean | object | any;
+	message: boolean;
+};
 
 export default function Contact() {
-	const [formInputs, setFormInputs] = useState({
-		name: '',
-		email: '',
-		subject: '',
-		message: '',
-	})
+	const [formInputs, setFormInputs] = useState<InputProps>({
+		name: "",
+		email: "",
+		subject: "",
+		message: "",
+	});
 	//   Form validation state
 	const [errors, setErrors] = useState<ErrorProps>({
 		name: true,
 		email: true,
-		subject: true,
 		message: true,
-	})
-	const [buttonText, setButtonText] = useState('Send')
-	const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+	});
+	const [buttonText, setButtonText] = useState("Send");
+	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
 	const handleChange = (e: any) => {
-		setFormInputs(values => ({
+		setFormInputs((values) => ({
 			...values,
 			[e.target.name]: e.target.value,
-		}))
-		setErrors(values => ({
+		}));
+		setErrors((values) => ({
 			...values,
 			[e.target.name]: false,
-		}))
-	}
+		}));
+	};
 
 	const isEmail = (email: string) =>
-		/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
+		/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
 
 	const formValidator = () => {
-		let tempErrors: object | any = {}
-		let isValid = true
+		let tempErrors: object | any = {};
+		let isValid = true;
 		if (!formInputs.name) {
-			tempErrors['name'] = true
-			isValid = false
+			tempErrors["name"] = true;
+			isValid = false;
 		}
 		if (!formInputs.email) {
-			tempErrors['email'] = {
+			tempErrors["email"] = {
 				isError: true,
-				errorMessage: 'Email is required',
-			}
-			isValid = false
+				errorMessage: "Email is required",
+			};
+			isValid = false;
 		} else if (!isEmail(formInputs.email)) {
-			tempErrors['email'] = {
+			tempErrors["email"] = {
 				isError: true,
-				errorMessage: 'Invalid email!',
-			}
-			isValid = false
-		}
-		if (!formInputs.subject) {
-			tempErrors['subject'] = true
-			isValid = false
-		}
-		if (!formInputs.message) {
-			tempErrors['message'] = true
-			isValid = false
+				errorMessage: "Invalid email!",
+			};
+			isValid = false;
 		}
 
-		setErrors({ ...tempErrors })
-		return isValid
-	}
+		if (!formInputs.message) {
+			tempErrors["message"] = true;
+			isValid = false;
+		}
+
+		setErrors({ ...tempErrors });
+		return isValid;
+	};
 
 	const handleSubmit = async (e: any) => {
-		e.preventDefault()
-		let isValid = formValidator()
+		e.preventDefault();
+		let isValid = formValidator();
 		if (!isValid) {
-			return
+			return;
 		}
 
-		const response = await fetch('/api', {
-			method: 'POST',
+		const response = await fetch("/api", {
+			method: "POST",
 			body: JSON.stringify(formInputs),
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
-		})
+		});
 
-		const res = await response.json()
+		const res = await response.json();
 
 		if (res.error) {
-			console.log(res.error, '<------ error')
-			setShowSuccessMessage(false)
-			setButtonText('Send')
-			return
-		} else console.log(res)
+			console.log(res.error, "<------ error");
+			setShowSuccessMessage(false);
+			setButtonText("Send");
+			return;
+		} else console.log(res);
 		setFormInputs({
-			name: '',
-			email: '',
-			subject: '',
-			message: '',
-		})
-		setShowSuccessMessage(true)
-		setButtonText('Send')
-	}
+			name: "",
+			email: "",
+			subject: "",
+			message: "",
+		});
+		setShowSuccessMessage(true);
+		setButtonText("Send");
+	};
 
 	const pageVariant = {
 		visible: {
 			opacity: 1,
-			transition: { type: 'spring', stiffness: 30, delay: 0.5, duration: 2 },
+			transition: { type: "spring", stiffness: 30, delay: 0.5, duration: 2 },
 		},
 		hidden: { opacity: 0 },
-	}
+	};
 
 	return (
 		<motion.main
 			variants={pageVariant}
-			initial='hidden'
-			whileInView='visible'
-			className={styles['contact-main']}
+			initial="hidden"
+			whileInView="visible"
+			className={styles["contact-main"]}
 		>
 			<ThemeProvider theme={theme}>
 				<Box
 					className={styles.box}
-					component='form'
+					component="form"
 					sx={{
-						'& .MuiTextField-root': {
+						"& .MuiTextField-root": {
 							m: 1,
-							width: '100%',
+							width: "100%",
 						},
 					}}
 					noValidate
-					autoComplete='off'
+					autoComplete="off"
 				>
-					<div className={styles['text-fields']}>
+					<div className={styles["text-fields"]}>
 						<TextField
 							className={styles.textField}
 							required
-							label='Name'
-							variant='filled'
-							name='name'
+							label="Name"
+							variant="filled"
+							name="name"
 							value={formInputs.name}
 							onChange={handleChange}
 							error={errors.name}
 							helperText={
-								errors['name'] ? 'Name is required' : 'Please enter your name'
+								errors["name"] ? "Name is required" : "Please enter your name"
 							}
 							InputProps={{ disableUnderline: true }}
 							sx={{
-								'& .MuiFormHelperText-root': {
-									color: 'var(--helper-text-color)',
-									fontSize: '14px',
-									fontWeight: '400',
+								"& .MuiFormHelperText-root": {
+									color: "var(--helper-text-color)",
+									fontSize: "14px",
+									fontWeight: "400",
 								},
 							}}
 						/>
 						<TextField
 							className={styles.textField}
 							required
-							label='Email'
-							variant='filled'
-							name='email'
+							label="Email"
+							variant="filled"
+							name="email"
 							value={formInputs.email}
 							onChange={handleChange}
 							error={errors.email}
 							helperText={
-								errors['email']
+								errors["email"]
 									? errors.email.errorMessage
-									: 'Please enter your email'
+									: "Please enter your email"
 							}
 							InputProps={{ disableUnderline: true }}
 							sx={{
-								'& .MuiFormHelperText-root': {
-									color: 'var(--helper-text-color)',
-									fontSize: '14px',
-									fontWeight: '400',
+								"& .MuiFormHelperText-root": {
+									color: "var(--helper-text-color)",
+									fontSize: "14px",
+									fontWeight: "400",
 								},
 							}}
 						/>
 						<TextField
 							className={styles.textField}
-							label='Subject'
-							variant='filled'
-							name='subject'
+							label="Subject"
+							variant="filled"
+							name="subject"
 							value={formInputs.subject}
 							onChange={handleChange}
-							helperText='Please enter the subject'
+							helperText="Please enter the subject"
 							InputProps={{ disableUnderline: true }}
 							sx={{
-								'& .MuiFormHelperText-root': {
-									color: 'var(--helper-text-color)',
-									fontSize: '14px',
-									fontWeight: '400',
+								"& .MuiFormHelperText-root": {
+									color: "var(--helper-text-color)",
+									fontSize: "14px",
+									fontWeight: "400",
 								},
 							}}
 						/>
 					</div>
-					<div className={styles['message-wrapper']}>
+					<div className={styles["message-wrapper"]}>
 						<TextField
 							required
 							className={`${styles.message} ${styles.textField}`}
-							label='Message'
-							variant='filled'
+							label="Message"
+							variant="filled"
 							multiline
 							minRows={5}
 							maxRows={10}
-							name='message'
+							name="message"
 							value={formInputs.message}
 							onChange={handleChange}
 							error={errors.message}
 							helperText={
-								errors['message']
-									? 'Message is required'
-									: 'Please enter your message'
+								errors["message"]
+									? "Message is required"
+									: "Please enter your message"
 							}
 							InputProps={{ disableUnderline: true }}
 							sx={{
-								'& .MuiFormHelperText-root': {
-									color: 'var(--helper-text-color)',
-									fontSize: '14px',
-									fontWeight: '400',
+								"& .MuiFormHelperText-root": {
+									color: "var(--helper-text-color)",
+									fontSize: "14px",
+									fontWeight: "400",
 								},
-								'& .Mui-error': {
-									color: '#f44336',
+								"& .Mui-error": {
+									color: "#f44336",
 								},
-								'& .MuiInputBase-root': {
-									color: 'var(--primary-text-color)',
-									borderBottom: '1px solid var(--primary-text-color)',
+								"& .MuiInputBase-root": {
+									color: "var(--primary-text-color)",
+									borderBottom: "1px solid var(--primary-text-color)",
 								},
 							}}
 						/>
 					</div>
 					<br />
-					<div className={styles['btn-container']}>
-						<button onClick={handleSubmit} className={`btn ${styles.send}`}>
+					<div className={styles["btn-container"]}>
+						<motion.button onClick={handleSubmit} className={styles.send} whileHover={{ scale: 1.1 }}>
 							{buttonText} &nbsp;
 							<FontAwesomeIcon
 								icon={faPaperPlane}
-								className={styles['fa-paper-plane']}
+								className={styles["fa-paper-plane"]}
 							/>
-						</button>
+						</motion.button>
 					</div>
 				</Box>
 			</ThemeProvider>
 		</motion.main>
-	)
+	);
 }
