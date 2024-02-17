93ce9cf9aae4844af84a27cfef2639a25457271f
Biruk Kebede <biruk.key@gmail.com>
2024-02-17 16:40:47 +0000

    Adding testimonials section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,7 +17,6 @@ import {
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
-import Reveal from '@/animations/reveal'
 import styles from '../styles/footer.module.css'
 
 type ContactProps = {
@@ -30,90 +29,81 @@ type ContactProps = {
 
 const Footer = ({ contact }: { contact: ContactProps }) => {
 	return (
-		<Reveal el='footer' scale={0} once duration={4} amount={0.2}>
-			<main className={styles['footer-main']}>
-				<div className={styles['nav-and-connect']}>
-					<div className={styles['navigation-links']}>
-						<h6 className={styles['footer-sub-title']}>
-							Site Navigation Links:
-						</h6>
-						<div className={styles['nav-links']}>
-							<Link href={'/'} className={styles['nav-link']}>
-								Home
-							</Link>
-							<Link href={'/services'} className={styles['nav-link']}>
-								Services
-							</Link>
-							<Link href={'/projects'} className={styles['nav-link']}>
-								Projects
-							</Link>
-							<Link href={'/about'} className={styles['nav-link']}>
-								About
-							</Link>
-							<Link href={'/contact'} className={styles['nav-link']}>
-								Contact
-							</Link>
-						</div>
+		<main className={styles['footer-main']}>
+			<div className={styles['nav-and-connect']}>
+				<div className={styles['navigation-links']}>
+					<h6 className={styles['footer-sub-title']}>Site Navigation Links:</h6>
+					<div className={styles['nav-links']}>
+						<Link href={'/'} className={styles['nav-link']}>
+							Home
+						</Link>
+						<Link href={'/services'} className={styles['nav-link']}>
+							Services
+						</Link>
+						<Link href={'/projects'} className={styles['nav-link']}>
+							Projects
+						</Link>
+						<Link href={'/about'} className={styles['nav-link']}>
+							About
+						</Link>
+						<Link href={'/contact'} className={styles['nav-link']}>
+							Contact
+						</Link>
 					</div>
 				</div>
-				<div className={styles.connect}>
-					<h6 className={styles['footer-sub-title']}>Connect with me on:</h6>
-					<div className={styles.brands}>
-						<a
-							href={contact.social_media[0]}
-							target='_blank'
-							rel='noreferrer'
-							className={styles.anchor}
-						>
-							<FontAwesomeIcon
-								icon={faGithub}
-								className={styles.github}
-								size='2xl'
-							/>
-						</a>
-						<a href={contact.social_media[1]} target='_blank' rel='noreferrer'>
-							<FontAwesomeIcon
-								icon={faLinkedinIn}
-								className={styles.linkedin}
-								size='2xl'
-							/>
-						</a>
-					</div>
+			</div>
+			<div className={styles.connect}>
+				<h6 className={styles['footer-sub-title']}>Connect with me on:</h6>
+				<div className={styles.brands}>
+					<a
+						href={contact.social_media[0]}
+						target='_blank'
+						rel='noreferrer'
+						className={styles.anchor}
+					>
+						<FontAwesomeIcon
+							icon={faGithub}
+							className={styles.github}
+							size='2xl'
+						/>
+					</a>
+					<a href={contact.social_media[1]} target='_blank' rel='noreferrer'>
+						<FontAwesomeIcon
+							icon={faLinkedinIn}
+							className={styles.linkedin}
+							size='2xl'
+						/>
+					</a>
 				</div>
+			</div>
 
-				<div className={styles['contact-info']}>
-					<div className={styles['contact-options']}>
-						<h6 className={styles['footer-sub-title']}>
-							More contact options:
-						</h6>
-						<div>
-							<p>
-								<FontAwesomeIcon icon={faEnvelope} className={styles.email} />{' '}
-								&nbsp; {contact.email}
-							</p>
-							<p>
-								<FontAwesomeIcon
-									icon={faMapLocationDot}
-									className={styles.location}
-								/>{' '}
-								&nbsp; {contact.post}
-							</p>
-							<p>
-								<FontAwesomeIcon
-									icon={faPhoneVolume}
-									className={styles.phone}
-								/>{' '}
-								&nbsp; {contact.phone}
-							</p>
-						</div>
+			<div className={styles['contact-info']}>
+				<div className={styles['contact-options']}>
+					<h6 className={styles['footer-sub-title']}>More contact options:</h6>
+					<div>
+						<p>
+							<FontAwesomeIcon icon={faEnvelope} className={styles.email} />{' '}
+							&nbsp; {contact.email}
+						</p>
+						<p>
+							<FontAwesomeIcon
+								icon={faMapLocationDot}
+								className={styles.location}
+							/>{' '}
+							&nbsp; {contact.post}
+						</p>
+						<p>
+							<FontAwesomeIcon icon={faPhoneVolume} className={styles.phone} />{' '}
+							&nbsp; {contact.phone}
+						</p>
 					</div>
 				</div>
-				<p className={styles.copyright}>
-					Copyright <FontAwesomeIcon icon={faCopyright} /> 2024, DB Freelance
-					Developer Ltd.
-				</p>
-			</main>
-		</Reveal>
+			</div>
+			<p className={styles.copyright}>
+				Copyright <FontAwesomeIcon icon={faCopyright} /> 2024, DB Freelance
+				Developer Ltd.
+			</p>
+		</main>
 	)
 }
 
